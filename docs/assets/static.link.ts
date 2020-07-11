2ff5448d35ca9a95c710fa8138d4f8982eaf39aa
Abhishek Yadav sabhis231 <sabhis231@gmail.com>
2020-07-11 19:39:07 +0530

    fixing unwanted files

--- docs/assets/static.link.ts
+++
@@ -1,2 +0,0 @@
-export const signUpApi = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=';
-export const signInApi = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=';
